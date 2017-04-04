3bc8774e135b3efc217aa03f6e955346b916de2b
Rafael <rapha.pse@outlook.com>
2017-04-04 20:54:49 -0300

    Adicionando Entidade Cliente - Funcionario e Usuário

---
+++ Drogaria/src/main/java/br/com/drogaria/domain/Usuario.java
@@ -0,0 +1,60 @@
+package br.com.drogaria.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@SuppressWarnings("serial")
+@Entity
+public class Usuario extends GenericDomain{
+	
+	@Column(nullable = false, length = 32)
+	private String senha;
+	
+	@Column(nullable = false)
+	private Character tipo;
+	
+	@Column(nullable = false)
+	private Boolean ativo;
+	
+	@OneToOne
+	@JoinColumn(nullable = false)
+	private Pessoa pessoa;
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Character getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(Character tipo) {
+		this.tipo = tipo;
+	}
+
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+	
+	
+	
+
+}
