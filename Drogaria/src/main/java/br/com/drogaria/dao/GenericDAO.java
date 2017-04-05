81482ffc3d69dcbb2eb7963f51a2bf27c4189467
Rafael <rapha.pse@outlook.com>
2017-04-05 14:28:25 -0300

    Criando metodo de listar com hibernate

--- Drogaria/src/main/java/br/com/drogaria/dao/GenericDAO.java
+++ Drogaria/src/main/java/br/com/drogaria/dao/GenericDAO.java
@@ -1,7 +1,9 @@
 package br.com.drogaria.dao;
 
 import java.lang.reflect.ParameterizedType;
+import java.util.List;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -10,37 +12,53 @@ import br.com.drogaria.util.HibernateUtil;
 public class GenericDAO<Entidade> {
 
 	private Class<Entidade> classe;
-	
+
 	@SuppressWarnings("unchecked")
 	public GenericDAO() {
-		this.classe = (Class<Entidade>)((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
-		
+		this.classe = (Class<Entidade>) ((ParameterizedType) getClass().getGenericSuperclass())
+				.getActualTypeArguments()[0];
+
 	}
-	
-	public void salvar(Entidade entidade){
-		
+
+	public void salvar(Entidade entidade) {
+
 		Session sessao = HibernateUtil.getFabricaDeSessao().openSession();
 		Transaction transacao = null;
-		
+
 		try {
-			
+
 			transacao = sessao.beginTransaction();
 			sessao.save(entidade);
 			transacao.commit();
-			
+
 		} catch (RuntimeException e) {
-			
-			if(transacao != null){
+
+			if (transacao != null) {
 				transacao.rollback();
 			}
-			
+
 			throw e;
+
+		} finally {
+
+			sessao.close();
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Entidade> listar(){
 		
-		}finally{
-			
+		Session sessao = HibernateUtil.getFabricaDeSessao().openSession();
+		
+		try {
+			Criteria consulta = sessao.createCriteria(classe);
+			List<Entidade> resultado = consulta.list();
+			return resultado;
+		} catch (RuntimeException e) {
+			throw e;
+		}finally {
 			sessao.close();
 		}
 	}
 
-	
 }
--- Drogaria/src/test/java/br/com/drogaria/dao/EstadoDAOTest.java
+++ Drogaria/src/test/java/br/com/drogaria/dao/EstadoDAOTest.java
@@ -1,24 +1,39 @@
 package br.com.drogaria.dao;
 
+import java.util.List;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
 import br.com.drogaria.domain.Estado;
 
 public class EstadoDAOTest {
-	
+
 	@Test
-	public void salvar(){
-		
+	@Ignore
+	public void salvar() {
+
 		Estado estado = new Estado();
-		
+
 		estado.setNome("Rio Grande do Sul");
 		estado.setSigla("RS");
-		
+
 		EstadoDAO dao = new EstadoDAO();
-		
+
 		dao.salvar(estado);
+
+	}
+	
+	@Test
+	public void listar(){
+		EstadoDAO dao = new EstadoDAO();
+		List<Estado> resultado = dao.listar();
 		
+		System.out.println("Total de resutlados: "+ resultado.size());
 		
+		for(Estado estado : resultado){
+			System.out.println(estado.getCodigo()+" "+estado.getSigla()+" - "+estado.getNome());
+		}
 	}
 
 }
