6034232ff5839da8714a19f56152678fb3b95b9d
Rafael <rapha.pse@outlook.com>
2017-04-05 16:20:41 -0300

    Criando metodos fabricanteDAO e aprimorando salvar e listar

--- Drogaria/src/test/java/br/com/drogaria/dao/FabricanteDAOTest.java
+++ Drogaria/src/test/java/br/com/drogaria/dao/FabricanteDAOTest.java
@@ -5,51 +5,73 @@ import java.util.List;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import br.com.drogaria.domain.Estado;
 import br.com.drogaria.domain.Fabricante;
 
 public class FabricanteDAOTest {
 
 	@Test
 	@Ignore
-	public void salvar(){
-		
+	public void salvar() {
+
 		Fabricante fabricante = new Fabricante();
 		fabricante.setDescricao("BAYER");
-		
+
 		Fabricante fb = new Fabricante();
 		fb.setDescricao("Hinode");
-		
+
 		FabricanteDAO dao = new FabricanteDAO();
-		
+
 		dao.salvar(fabricante);
 		dao.salvar(fb);
 	}
-	
+
 	@Test
 	@Ignore
-	public void listar(){
+	public void listar() {
 		FabricanteDAO dao = new FabricanteDAO();
-		
+
 		List<Fabricante> resultado = dao.listar();
-		
-		System.out.println("Total de Resultados: "+resultado.size());
-		
-		for(Fabricante fabricante: resultado){
-			System.out.println(fabricante.getCodigo()+" "+fabricante.getDescricao());
+
+		System.out.println("Total de Resultados: " + resultado.size());
+
+		for (Fabricante fabricante : resultado) {
+			System.out.println(fabricante.getCodigo() + " " + fabricante.getDescricao());
 		}
 	}
+
 	@Test
-	public void buscar(){
-		
+	@Ignore
+	public void buscar() {
+
 		Long codigo = 2L;
-		
+
 		FabricanteDAO dao = new FabricanteDAO();
 		Fabricante fb = dao.buscar(codigo);
-		
-		if(fb == null){
+
+		if (fb == null) {
 			System.out.println("Nenhum resultado");
 		}
-		
-		System.out.println(fb.getCodigo()+" "+fb.getDescricao());
+
+		System.out.println(fb.getCodigo() + " " + fb.getDescricao());
+	}
+
+	
+	@Test
+	public void excluir() {
+
+		Long codigo = 4L;
+
+		FabricanteDAO dao = new FabricanteDAO();
+		Fabricante fabricante = dao.buscar(codigo);
+
+		if (fabricante == null) {
+			System.out.println("Nenhum resultado encontrado");
+		}else {
+			dao.excluir(fabricante);
+			System.out.println("Removido com sucesso:");
+			System.out.println(fabricante.getCodigo() + " " + fabricante.getDescricao());
+		}
+
 	}
 }
