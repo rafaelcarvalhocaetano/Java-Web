7a71935d05870d092c298c916e8278ce6be2e1d3
Rafael <rapha.pse@outlook.com>
2017-04-05 14:43:19 -0300

    Criando metodos fabricanteDAO e aprimorando salvar e listar

--- Drogaria/src/test/java/br/com/drogaria/dao/FabricanteDAOTest.java
+++ Drogaria/src/test/java/br/com/drogaria/dao/FabricanteDAOTest.java
@@ -1,5 +1,8 @@
 package br.com.drogaria.dao;
 
+import java.util.List;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
 import br.com.drogaria.domain.Fabricante;
@@ -7,6 +10,7 @@ import br.com.drogaria.domain.Fabricante;
 public class FabricanteDAOTest {
 
 	@Test
+	@Ignore
 	public void salvar(){
 		
 		Fabricante fabricante = new Fabricante();
@@ -20,4 +24,18 @@ public class FabricanteDAOTest {
 		dao.salvar(fabricante);
 		dao.salvar(fb);
 	}
+	
+	@Test
+	public void listar(){
+		FabricanteDAO dao = new FabricanteDAO();
+		
+		List<Fabricante> resultado = dao.listar();
+		
+		System.out.println("Total de Resultados: "+resultado.size());
+		
+		for(Fabricante fabricante: resultado){
+			System.out.println(fabricante.getCodigo()+" "+fabricante.getDescricao());
+		}
+	}
+	
 }
