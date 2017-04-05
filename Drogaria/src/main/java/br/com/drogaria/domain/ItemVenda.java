3f072af36d1d3e233a7ad9fe6d71f34df4e0aadb
Rafael <rapha.pse@outlook.com>
2017-04-04 21:24:58 -0300

    Adicionando Entidade Cliente - Funcionario - Usuário - Venda e ItemVenda

---
+++ Drogaria/src/main/java/br/com/drogaria/domain/ItemVenda.java
@@ -0,0 +1,65 @@
+package br.com.drogaria.domain;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@SuppressWarnings("serial")
+@Entity
+public class ItemVenda extends GenericDomain{
+	
+	@Column(nullable = false)
+	private Short quantidade;
+	
+	@Column(nullable = false, precision = 7, scale = 2)
+	private BigDecimal valorParcial;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Produto produto;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Venda venda;
+
+	public Short getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Short quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public BigDecimal getValorParcial() {
+		return valorParcial;
+	}
+
+	public void setValorParcial(BigDecimal valorParcial) {
+		this.valorParcial = valorParcial;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public Venda getVenda() {
+		return venda;
+	}
+
+	public void setVenda(Venda venda) {
+		this.venda = venda;
+	}
+	
+	
+	
+	
+	
+
+}
