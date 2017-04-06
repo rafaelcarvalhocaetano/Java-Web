61a0704e12b9352ff4d8f57403dcb1fa43b1db40
Rafael <rapha.pse@outlook.com>
2017-04-06 14:47:58 -0300

    Criando Salvar, editar e excluir do produtoDAO

--- Drogaria/src/test/java/br/com/drogaria/dao/ProdutoDAOTest.java
+++ Drogaria/src/test/java/br/com/drogaria/dao/ProdutoDAOTest.java
@@ -2,6 +2,7 @@ package br.com.drogaria.dao;
 
 import java.math.BigDecimal;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import br.com.drogaria.domain.Fabricante;
@@ -10,6 +11,7 @@ import br.com.drogaria.domain.Produto;
 public class ProdutoDAOTest {
 	
 	@Test
+	@Ignore
 	public void salvar(){
 		Produto produto = new Produto();
 		
@@ -25,6 +27,40 @@ public class ProdutoDAOTest {
 		ProdutoDAO dao2 = new ProdutoDAO();
 		dao2.salvar(produto);
 		
+	}
+	
+	@Test
+	@Ignore
+	public void excluir(){
+		
+		Long codigo = 1L;
+		
+		ProdutoDAO dao = new ProdutoDAO();
+		Produto produto = dao.buscar(codigo);
+		
+		if(produto == null){
+			System.out.println("Nenhum produto encontrado...");
+		}else{
+			dao.excluir(produto);
+			System.out.println("Produto removido com sucesso...");
+		}
+	}
+	@Test
+	public void editar(){
+		Long codigo = 2L;
+		
+		ProdutoDAO dao = new ProdutoDAO();
+		Produto produto = dao.buscar(codigo);
+		
+		if(produto == null){
+			System.out.println("Nenhuma produto encontrado....");
+		}else{
+			System.out.println(produto.getCodigo()+" "+produto.getDescricao()+" "+produto.getQuantidade()+" "+produto.getFabricante()+" "+produto.getPreco());
+			produto.setDescricao("Novalgina com 30 mg");
+			dao.editar(produto);
+			System.out.println(produto.getCodigo()+" "+produto.getDescricao()+" "+produto.getQuantidade()+" "+produto.getFabricante()+" "+produto.getPreco());
+			
+		}
 	}
 
 }
