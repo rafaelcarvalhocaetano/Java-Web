7087767f2ee7b28c3b699ca0fefd18f7fd5b63b4
Rafael <rapha.pse@outlook.com>
2017-04-05 21:14:56 -0300

    Criando camada salvar, listar

--- Drogaria/src/test/java/br/com/drogaria/dao/CidadeDAOTest.java
+++ Drogaria/src/test/java/br/com/drogaria/dao/CidadeDAOTest.java
@@ -1,24 +1,41 @@
 package br.com.drogaria.dao;
 
+import java.util.List;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
 import br.com.drogaria.domain.Cidade;
 import br.com.drogaria.domain.Estado;
 
 public class CidadeDAOTest {
-	
+
 	@Test
-	public void salvar(){
+	@Ignore
+	public void salvar() {
 		EstadoDAO dao = new EstadoDAO();
 		Estado estado = dao.buscar(1L);
-		
+
 		Cidade cidade = new Cidade();
 		cidade.setNome("Osasco");
 		cidade.setEstado(estado);
-		
+
 		CidadeDAO cidadeDAO = new CidadeDAO();
 		cidadeDAO.salvar(cidade);
-		
+
+	}
+
+	@Test
+	public void listar() {
+		CidadeDAO dao = new CidadeDAO();
+		List<Cidade> cidades = dao.listar();
+
+		for (Cidade cd : cidades) {
+
+			System.out.println("Código da cidade: " + cd.getCodigo());
+			System.out.println("Nome da cidade: " + cd.getNome());
+			System.out.println("Código do Estado: " + cd.getEstado().getCodigo());
+		}
 	}
 
 }
