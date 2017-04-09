9140318dbdc890d5b708540189f7a64c516b549a
Rafael <rapha.pse@outlook.com>
2017-04-09 19:41:53 -0300

    Criando Cliente DAO Test

--- Drogaria/src/test/java/br/com/drogaria/dao/ClienteDAOTest.java
+++ Drogaria/src/test/java/br/com/drogaria/dao/ClienteDAOTest.java
@@ -1,5 +1,25 @@
 package br.com.drogaria.dao;
 
+import org.junit.Test;
+
+import br.com.drogaria.domain.Cliente;
+import br.com.drogaria.domain.Pessoa;
+
 public class ClienteDAOTest {
+	
+	@Test
+	public void salvar(){
+		
+		Cliente cliente = new Cliente();
+		
+		cliente.setDataCadastro(dataCadastro);
+		cliente.setLiberado(true);
+		
+		Long codigo = 2L;
+		PessoaDAO dao = new PessoaDAO();
+		Pessoa pessoa = dao.buscar(codigo);
+				
+		cliente.setPessoa(pessoa);
+	}
 
 }
