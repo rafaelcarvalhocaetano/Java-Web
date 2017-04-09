d740cf969492ae2645628915660a1f24728fd579
Rafael <rapha.pse@outlook.com>
2017-04-09 20:37:54 -0300

    Criando Usuário DAO Test

--- Drogaria/src/test/java/br/com/drogaria/dao/UsuarioDAOTest.java
+++ Drogaria/src/test/java/br/com/drogaria/dao/UsuarioDAOTest.java
@@ -1,43 +1,57 @@
 package br.com.drogaria.dao;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
+import br.com.drogaria.domain.Cliente;
 import br.com.drogaria.domain.Pessoa;
 import br.com.drogaria.domain.Usuario;
 
 public class UsuarioDAOTest {
-	
-	
+
 	@Test
-	public void salvar(){
-		
+	@Ignore
+	public void salvar() {
+
 		Long codigo = 2L;
-		
+
 		PessoaDAO pd = new PessoaDAO();
-		Pessoa pessoa = pd.buscar(codigo); //usando a referencia do codigo para os dois tipos
-		
+		Pessoa pessoa = pd.buscar(codigo); // usando a referencia do codigo para os dois tipos
+
 		System.out.println(pessoa.getNome());
 		System.out.println(pessoa.getCpf());
-		
-		
+
 		Usuario us = new Usuario();
-	
+
 		us.setPessoa(pessoa);
 		us.setAtivo(true);
 		us.setSenha("2222222");
 		us.setTipo('M');
-		
+
 		UsuarioDAO dao = new UsuarioDAO();
 		dao.salvar(us);
-		
+
 		System.out.println(us.getAtivo());
 		System.out.println(us.getSenha());
 		System.out.println(us.getCodigo());
 		System.out.println(us.getTipo());
-		
 
+	}
+	@SuppressWarnings("unused")
+	@Test
+	public void buscar(){
 		
-
+		Long codigo = 4L;
+		
+		ClienteDAO dao = new ClienteDAO();
+		Cliente ca = new Cliente();
+		
+		if(ca == null){
+			System.out.println("Nenhum usuário encontrado");
+		}else{
+			dao.buscar(codigo);
+			//System.out.println(ca.getCodigo()+" "+ca.getDataCadastro()+" "+ca.getLiberado()+" "+ca.getPessoa().getNome());
+		}
 	}
 
 }
