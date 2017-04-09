622074f74479c73aa6da7a45b848122c5f4331b3
Rafael <rapha.pse@outlook.com>
2017-04-09 19:51:38 -0300

    Criando Cliente DAO Test

--- Drogaria/src/test/java/br/com/drogaria/dao/ClienteDAOTest.java
+++ Drogaria/src/test/java/br/com/drogaria/dao/ClienteDAOTest.java
@@ -1,5 +1,9 @@
 package br.com.drogaria.dao;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.junit.Test;
 
 import br.com.drogaria.domain.Cliente;
@@ -8,18 +12,23 @@ import br.com.drogaria.domain.Pessoa;
 public class ClienteDAOTest {
 	
 	@Test
-	public void salvar(){
+	public void salvar() throws ParseException{
 		
 		Cliente cliente = new Cliente();
 		
-		cliente.setDataCadastro(dataCadastro);
-		cliente.setLiberado(true);
+		cliente.setDataCadastro(new SimpleDateFormat("dd/mm/yyyy").parse("09/04/2017"));
+		cliente.setLiberado(false);
 		
-		Long codigo = 2L;
+		Long codigo = 1L;
 		PessoaDAO dao = new PessoaDAO();
 		Pessoa pessoa = dao.buscar(codigo);
 				
 		cliente.setPessoa(pessoa);
+		
+		ClienteDAO clienteDAO = new ClienteDAO();
+		clienteDAO.salvar(cliente);
+		
+		System.out.println("Cliente Salvo com sucesso ...");
 	}
 
 }
