ad02980bc58344a33178a71c81b0754b27f2f3ee
Rafael <rapha.pse@outlook.com>
2017-04-09 20:31:55 -0300

    Criando Usuário DAO Test

--- Drogaria/src/test/java/br/com/drogaria/dao/UsuarioDAOTest.java
+++ Drogaria/src/test/java/br/com/drogaria/dao/UsuarioDAOTest.java
@@ -1,5 +1,43 @@
 package br.com.drogaria.dao;
 
+import org.junit.Test;
+
+import br.com.drogaria.domain.Pessoa;
+import br.com.drogaria.domain.Usuario;
+
 public class UsuarioDAOTest {
+	
+	
+	@Test
+	public void salvar(){
+		
+		Long codigo = 2L;
+		
+		PessoaDAO pd = new PessoaDAO();
+		Pessoa pessoa = pd.buscar(codigo); //usando a referencia do codigo para os dois tipos
+		
+		System.out.println(pessoa.getNome());
+		System.out.println(pessoa.getCpf());
+		
+		
+		Usuario us = new Usuario();
+	
+		us.setPessoa(pessoa);
+		us.setAtivo(true);
+		us.setSenha("2222222");
+		us.setTipo('M');
+		
+		UsuarioDAO dao = new UsuarioDAO();
+		dao.salvar(us);
+		
+		System.out.println(us.getAtivo());
+		System.out.println(us.getSenha());
+		System.out.println(us.getCodigo());
+		System.out.println(us.getTipo());
+		
+
+		
+
+	}
 
 }
