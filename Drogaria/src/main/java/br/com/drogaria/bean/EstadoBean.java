e3690932cc79002d4d439fbbdfd1543fca5abfb3
Rafael <rapha.pse@outlook.com>
2017-04-12 09:49:02 -0300

    Adicionando mensagem com growl na View estado e criando Controller

--- Drogaria/src/main/java/br/com/drogaria/bean/EstadoBean.java
+++ Drogaria/src/main/java/br/com/drogaria/bean/EstadoBean.java
@@ -1,12 +1,20 @@
 package br.com.drogaria.bean;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
 @ManagedBean
 public class EstadoBean {
 	
 	public void salvar(){
-		System.out.println("Programação web com java");
+		String texto = "Programação web com java";
+		FacesMessage mensagem = new FacesMessage(FacesMessage.SEVERITY_INFO, texto, texto);
+		
+		FacesContext contexto = FacesContext.getCurrentInstance();
+		contexto.addMessage(null, mensagem);
+		
+		
 	}
 
 }
