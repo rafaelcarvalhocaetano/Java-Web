dcdc46dcaf69cdba9669c0b59faae029b8e73d97
Rafael <rapha.pse@outlook.com>
2017-04-11 22:30:59 -0300

    Criando Estado - Controller

--- Drogaria/src/main/java/br/com/drogaria/bean/EstadoBean.java
+++ Drogaria/src/main/java/br/com/drogaria/bean/EstadoBean.java
@@ -1,5 +1,12 @@
 package br.com.drogaria.bean;
 
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
 public class EstadoBean {
+	
+	public void salvar(){
+		System.out.println("Programação web com java");
+	}
 
 }
