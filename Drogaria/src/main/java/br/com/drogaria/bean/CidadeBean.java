36500aa8fc2b67bf072461d07252db61415b9192
Rafael <rapha.pse@outlook.com>
2017-04-17 15:31:33 -0300

    Criando classe Cidade do CONTROLLER

---
+++ Drogaria/src/main/java/br/com/drogaria/bean/CidadeBean.java
@@ -0,0 +1,13 @@
+package br.com.drogaria.bean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@ViewScoped
+public class CidadeBean implements Serializable{
+
+}
