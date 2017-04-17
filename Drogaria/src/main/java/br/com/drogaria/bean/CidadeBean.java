1bcb5a126d2ffffcb90c3db382090e7ae2b9efa9
Rafael <rapha.pse@outlook.com>
2017-04-17 16:10:49 -0300

    Ordenando e criando componentes da Cidade do CONTROLLER e ligando com a VIEW

--- Drogaria/src/main/java/br/com/drogaria/bean/CidadeBean.java
+++ Drogaria/src/main/java/br/com/drogaria/bean/CidadeBean.java
@@ -10,44 +10,67 @@ import javax.faces.bean.ViewScoped;
 import org.omnifaces.util.Messages;
 
 import br.com.drogaria.dao.CidadeDAO;
+import br.com.drogaria.dao.EstadoDAO;
 import br.com.drogaria.domain.Cidade;
+import br.com.drogaria.domain.Estado;
 
 @SuppressWarnings("serial")
 @ManagedBean
 @ViewScoped
-public class CidadeBean implements Serializable{
-	
+public class CidadeBean implements Serializable {
+
 	private Cidade cidade;
 	private List<Cidade> cidades;
-	
+	private List<Estado> estados;
+
 	public List<Cidade> getCidades() {
 		return cidades;
 	}
-	
+
 	public void setCidades(List<Cidade> cidades) {
 		this.cidades = cidades;
 	}
-	
+
 	public Cidade getCidade() {
 		return cidade;
 	}
+
 	public void setCidade(Cidade cidade) {
 		this.cidade = cidade;
 	}
-	
+
+	public List<Estado> getEstados() {
+		return estados;
+	}
+
+	public void setEstados(List<Estado> estados) {
+		this.estados = estados;
+	}
+
 	@PostConstruct
-	public void listar(){
+	public void listar() {
 		try {
 			CidadeDAO dao = new CidadeDAO();
 			cidades = dao.listar();
-			
+
 		} catch (RuntimeException e) {
 			Messages.addGlobalError("Erro ao editar a cidade");
 			e.printStackTrace();
 		}
 	}
-	public void novo(){
-		cidade = new Cidade();
+
+	public void novo() {
+
+		try {
+			cidade = new Cidade();
+
+			EstadoDAO dao = new EstadoDAO();
+
+			estados = dao.listar();
+		} catch (RuntimeException e) {
+			Messages.addGlobalError("Erro ao gerar uma nova cidade");
+			e.printStackTrace();
+		}
 	}
 
 }
