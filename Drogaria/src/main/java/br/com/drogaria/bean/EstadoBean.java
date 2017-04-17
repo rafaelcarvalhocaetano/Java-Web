6b9ab928a212206f3a997226c6d00a1c1e5ab942
Rafael <rapha.pse@outlook.com>
2017-04-17 14:55:32 -0300

    Errata VIEW junto com o CONTROLLER

--- Drogaria/src/main/java/br/com/drogaria/bean/EstadoBean.java
+++ Drogaria/src/main/java/br/com/drogaria/bean/EstadoBean.java
@@ -19,33 +19,34 @@ import br.com.drogaria.domain.Estado;
 @SuppressWarnings("serial")
 @ManagedBean
 @ViewScoped
-public class EstadoBean implements Serializable{
-	
+public class EstadoBean implements Serializable {
+
 	private List<Estado> estados;
-	
+
 	public List<Estado> getEstados() {
 		return estados;
 	}
-	
+
 	public void setEstados(List<Estado> estados) {
 		this.estados = estados;
 	}
-	
+
 	private Estado estado;
-	
+
 	public Estado getEstado() {
 		return estado;
 	}
+
 	public void setEstado(Estado estado) {
 		this.estado = estado;
 	}
-	
-	public void novo(){
+
+	public void novo() {
 		estado = new Estado();
 	}
-	
+
 	@PostConstruct
-	public void listar(){
+	public void listar() {
 		try {
 			EstadoDAO dao = new EstadoDAO();
 			estados = dao.listar();
@@ -53,32 +54,34 @@ public class EstadoBean implements Serializable{
 			Messages.addGlobalError("Erro ao acessar o Estado");
 		}
 	}
-	
-	public void salvar(){
-		
-		try{
-		EstadoDAO dao = new EstadoDAO();
-		dao.salvar(estado);
-		
-		novo();
-		
-		Messages.addGlobalInfo("Estado salvo com sucesso");
-		}catch(RuntimeException e){
+
+	public void salvar() {
+
+		try {
+			EstadoDAO dao = new EstadoDAO();
+			dao.salvar(estado);
+
+			estado = new Estado();
+			estados = dao.listar();
+
+			Messages.addGlobalInfo("Estado salvo com sucesso");
+		} catch (RuntimeException e) {
 			Messages.addGlobalError("Ocorreu um erro ao salvar o Estado");
 			e.printStackTrace();
 		}
 		/*
-		String texto = "Programação web com java";
-		FacesMessage mensagem = new FacesMessage(FacesMessage.SEVERITY_INFO, texto, texto);
-		
-		FacesContext contexto = FacesContext.getCurrentInstance();
-		contexto.addMessage(null, mensagem);
-		*/
-		
+		 * String texto = "Programação web com java"; FacesMessage mensagem =
+		 * new FacesMessage(FacesMessage.SEVERITY_INFO, texto, texto);
+		 * 
+		 * FacesContext contexto = FacesContext.getCurrentInstance();
+		 * contexto.addMessage(null, mensagem);
+		 */
+
 	}
-	public void excluir(ActionEvent event){
+
+	public void excluir(ActionEvent event) {
 		estado = (Estado) event.getComponent().getAttributes().get("estadoSelecionado");
-		Messages.addGlobalInfo("Nome: "+estado.getNome()+" Sigla: "+estado.getSigla());
+		Messages.addGlobalInfo("Nome: " + estado.getNome() + " Sigla: " + estado.getSigla());
 	}
 
 }
