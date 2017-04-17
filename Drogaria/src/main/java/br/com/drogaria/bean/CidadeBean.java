f05c1b123f187ee89eff91ad19e16feb55f1541e
Rafael <rapha.pse@outlook.com>
2017-04-17 15:33:52 -0300

    Criando classe Cidade do CONTROLLER e ligando com a VIEW

--- Drogaria/src/main/java/br/com/drogaria/bean/CidadeBean.java
+++ Drogaria/src/main/java/br/com/drogaria/bean/CidadeBean.java
@@ -1,13 +1,28 @@
 package br.com.drogaria.bean;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+
+import br.com.drogaria.domain.Cidade;
 
 @SuppressWarnings("serial")
 @ManagedBean
 @ViewScoped
 public class CidadeBean implements Serializable{
+	
+	private List<Cidade> cidades;
+	
+	public List<Cidade> getCidades() {
+		return cidades;
+	}
+	
+	public void setCidades(List<Cidade> cidades) {
+		this.cidades = cidades;
+	}
+	
+	
 
 }
