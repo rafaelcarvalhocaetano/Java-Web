510be1eaca3dc55bf7a062a10edc1403b55c80bd
Rafael <rapha.pse@outlook.com>
2017-04-18 14:25:13 -0300

    Criando metodo novo produtobean

--- Drogaria/src/main/java/br/com/drogaria/bean/ProdutoBean.java
+++ Drogaria/src/main/java/br/com/drogaria/bean/ProdutoBean.java
@@ -1,13 +1,66 @@
 package br.com.drogaria.bean;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+
+import org.omnifaces.util.Messages;
+
+import br.com.drogaria.dao.FabricanteDAO;
+import br.com.drogaria.domain.Fabricante;
+import br.com.drogaria.domain.Produto;
 
 @SuppressWarnings("serial")
 @ManagedBean
 @ViewScoped
 public class ProdutoBean implements Serializable{
+	
+	private Produto produto;
+	private List<Produto> produtos;
+	private List<Fabricante> fabricantes;
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+	
+	public List<Fabricante> getFabricantes() {
+		return fabricantes;
+	}
+	
+	public void setFabricantes(List<Fabricante> fabricantes) {
+		this.fabricantes = fabricantes;
+	}
+	
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	
+	public Produto getProduto() {
+		return produto;
+	}
+	
+	@PostConstruct
+	public void novo(){
+		
+		try {
+			
+			produto = new Produto();
+			
+			FabricanteDAO dao = new FabricanteDAO();
+			fabricantes = dao.listar();
+			
+			Messages.addGlobalInfo("Produto Salvo com sucesso");
+		} catch (RuntimeException e) {
+			Messages.addGlobalError("Erro ao criar novo produto");
+			e.printStackTrace();
+		}
+		
+	}
 
 }
