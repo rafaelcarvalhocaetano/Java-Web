743f091e399fa661fc229715636d10c28a130e02
Rafael <rapha.pse@outlook.com>
2017-04-18 10:54:16 -0300

    Gerendo equals e hashCode no genericdomain

--- Drogaria/src/main/java/br/com/drogaria/domain/GenericDomain.java
+++ Drogaria/src/main/java/br/com/drogaria/domain/GenericDomain.java
@@ -29,6 +29,31 @@ public class GenericDomain implements Serializable{
 		return String.format("%s[codigo=%d]", getClass().getSimpleName(), getCodigo());
 		
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GenericDomain other = (GenericDomain) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
 	
 	
 	
