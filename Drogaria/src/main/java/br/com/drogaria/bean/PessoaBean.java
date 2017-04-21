ba179ec400205d7260f3563558d5dffc6373829d
Rafael <rapha.pse@outlook.com>
2017-04-21 20:53:10 -0300

    Criando PessoaBean do controller e adicionando o metodo listar, novo e salvar

--- Drogaria/src/main/java/br/com/drogaria/bean/PessoaBean.java
+++ Drogaria/src/main/java/br/com/drogaria/bean/PessoaBean.java
@@ -18,52 +18,57 @@ import br.com.drogaria.domain.Pessoa;
 @SuppressWarnings("serial")
 @ManagedBean
 @ViewScoped
-public class PessoaBean implements Serializable{
-	
+public class PessoaBean implements Serializable {
+
 	private Pessoa pessoa;
 	private List<Pessoa> pessoas;
 	private List<Cidade> cidades;
-	
+
 	public Pessoa getPessoa() {
 		return pessoa;
 	}
+
 	public void setPessoa(Pessoa pessoa) {
 		this.pessoa = pessoa;
 	}
+
 	public List<Pessoa> getPessoas() {
 		return pessoas;
 	}
+
 	public void setPessoas(List<Pessoa> pessoas) {
 		this.pessoas = pessoas;
 	}
-	
+
 	public List<Cidade> getCidades() {
 		return cidades;
 	}
+
 	public void setCidades(List<Cidade> cidades) {
 		this.cidades = cidades;
 	}
-	
+
 	@PostConstruct
-	public void listar(){
-		
+	public void listar() {
+
 		try {
-			
+
 			PessoaDAO dao = new PessoaDAO();
 			pessoas = dao.listar();
-			
+
 		} catch (RuntimeException e) {
 			Messages.addGlobalError("Erro ao listar uma pessoa");
 		}
 	}
+
 	public void novo() {
 		try {
-			
+
 			pessoa = new Pessoa();
-			
+
 			PessoaDAO dao = new PessoaDAO();
 			pessoas = dao.listar();
-			
+
 		} catch (RuntimeException e) {
 			Messages.addGlobalError("Erro ao tentar adicionar uma nova Pessoa");
 		}
@@ -72,21 +77,32 @@ public class PessoaBean implements Serializable{
 
 	public void editar(ActionEvent evento) {
 		
+		try {
+			
+			pessoa = (Pessoa) evento.getComponent().getAttributes().get("pessoaSelecionada");
+			
+			PessoaDAO dao = new PessoaDAO();
+			pessoas = dao.listar();
+			
+		} catch (RuntimeException e) {
+			Messages.addGlobalError("Erro ao editar Pessoa");
+			e.printStackTrace();
+		}
 
 	}
 
 	public void salvar() {
 		try {
-			
+
 			PessoaDAO dao = new PessoaDAO();
 			dao.merge(pessoa);
-			
+
 			CidadeDAO cdao = new CidadeDAO();
 			cidades = cdao.listar();
-			
+
 			pessoa = new Pessoa();
 			pessoas = dao.listar();
-			
+
 		} catch (RuntimeException e) {
 			Messages.addGlobalError("Erro ao tentar salvar uma nova Pessoa");
 			e.printStackTrace();
@@ -95,10 +111,19 @@ public class PessoaBean implements Serializable{
 	}
 
 	public void excluir(ActionEvent evento) {
+		try {
+			pessoa = (Pessoa) evento.getComponent().getAttributes().get("pessoasSelecionadas");
+
+			PessoaDAO dao = new PessoaDAO();
+			dao.excluir(pessoa);
+
+			pessoas = dao.listar();
+
+			Messages.addGlobalInfo("Pessoa excluida com sucesso");
+		} catch (RuntimeException e) {
+			Messages.addGlobalError("Erro ao tentar excluir pessoa");
+		}
 
 	}
-	
-	
-	
 
 }
