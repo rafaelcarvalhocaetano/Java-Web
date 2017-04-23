8d6fd57f0ccee37df40c3b74ce64818bf8ad9907
Rafael <rapha.pse@outlook.com>
2017-04-23 18:52:49 -0300

    Criando Classe Cliente do Controller e vinculando com View cliente

---
+++ Drogaria/src/main/java/br/com/drogaria/bean/ClienteBean.java
@@ -0,0 +1,15 @@
+package br.com.drogaria.bean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@ViewScoped
+public class ClienteBean implements Serializable{
+	
+	
+
+}
