64bc328facc8e9869f9920bbc4ff8d27b08f352f
Rafael <rapha.pse@outlook.com>
2017-04-23 19:01:14 -0300

    Criando Classe Cliente do Controller e vinculando com View cliente

--- Drogaria/src/main/java/br/com/drogaria/bean/ClienteBean.java
+++ Drogaria/src/main/java/br/com/drogaria/bean/ClienteBean.java
@@ -1,14 +1,43 @@
 package br.com.drogaria.bean;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+
+import org.omnifaces.util.Messages;
+
+import br.com.drogaria.dao.ClienteDAO;
+import br.com.drogaria.domain.Cliente;
 
 @SuppressWarnings("serial")
 @ManagedBean
 @ViewScoped
 public class ClienteBean implements Serializable{
+	
+	private List<Cliente> clientes;
+
+	public List<Cliente> getClientes() {
+		return clientes;
+	}
+
+	public void setClientes(List<Cliente> clientes) {
+		this.clientes = clientes;
+	}
+	
+	@PostConstruct
+	public void listar(){
+		try {
+			ClienteDAO dao = new ClienteDAO();
+			clientes = dao.listar("dataCadastro");
+			
+		} catch (RuntimeException e) {
+			Messages.addGlobalError("Erro ao listar clientes");
+			e.printStackTrace();
+		}
+	}
 	
 	
 
