d1c271f4af51b28a5a0db77a4b3d3347cfc939fd
Rafael <rapha.pse@outlook.com>
2017-04-24 18:39:05 -0300

    Adicionando rest do conteudo sobre RESTFull

--- Drogaria/src/main/java/br/com/drogaria/service/FabricanteService.java
+++ Drogaria/src/main/java/br/com/drogaria/service/FabricanteService.java
@@ -2,7 +2,10 @@ package br.com.drogaria.service;
 
 import java.util.List;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+
+import com.google.gson.Gson;
 
 import br.com.drogaria.dao.FabricanteDAO;
 import br.com.drogaria.domain.Fabricante;
@@ -11,8 +14,14 @@ import br.com.drogaria.domain.Fabricante;
 @Path("fabricante")
 public class FabricanteService {
 
-	public void listar(){
+	@GET
+	public String listar(){
 		FabricanteDAO dao = new FabricanteDAO();
 		List<Fabricante> fabricantes = dao.listar("descricao");
+		
+		Gson gson = new Gson();
+		String json = gson.toJson(fabricantes);
+		
+		return json;
 	}
 }
