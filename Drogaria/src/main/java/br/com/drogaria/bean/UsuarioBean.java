4e3917601dc4bc4acffe3d8a3737eff6859ca82a
Rafael <rapha.pse@outlook.com>
2017-04-24 12:50:39 -0300

    Criando Controller e view do usuário

---
+++ Drogaria/src/main/java/br/com/drogaria/bean/UsuarioBean.java
@@ -0,0 +1,42 @@
+package br.com.drogaria.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import br.com.drogaria.domain.Pessoa;
+import br.com.drogaria.domain.Usuario;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@ViewScoped
+public class UsuarioBean implements Serializable{
+	
+	private Usuario usuario;	
+	private List<Usuario> usuarios;
+	private List<Pessoa> pessoas;
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	public List<Usuario> getUsuarios() {
+		return usuarios;
+	}
+	public void setUsuarios(List<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+	public List<Pessoa> getPessoas() {
+		return pessoas;
+	}
+	public void setPessoas(List<Pessoa> pessoas) {
+		this.pessoas = pessoas;
+	}
+	
+	
+
+}
