586e800d69f2d135370af8cb31575cf4825fc660
Rafael <rapha.pse@outlook.com>
2017-04-24 18:24:34 -0300

    Adicionando dependencia do gson

--- Drogaria/src/main/java/br/com/drogaria/service/FabricanteService.java
+++ Drogaria/src/main/java/br/com/drogaria/service/FabricanteService.java
@@ -1,9 +1,18 @@
 package br.com.drogaria.service;
 
+import java.util.List;
+
 import javax.ws.rs.Path;
+
+import br.com.drogaria.dao.FabricanteDAO;
+import br.com.drogaria.domain.Fabricante;
 
 //http://localhost:8080/Drogaria/rest/fabricante
 @Path("fabricante")
 public class FabricanteService {
 
+	public void listar(){
+		FabricanteDAO dao = new FabricanteDAO();
+		List<Fabricante> fabricantes = dao.listar("descricao");
+	}
 }
