c934745b6753aad8fd556ac1bed7a3a325f60f0d
Rafael <rapha.pse@outlook.com>
2017-04-24 17:18:53 -0300

    Aniciando o projeto com RESTFull - jersey

---
+++ Drogaria/src/main/java/br/com/drogaria/service/DrogariaService.java
@@ -0,0 +1,9 @@
+package br.com.drogaria.service;
+
+import javax.ws.rs.Path;
+
+//http://localhost:8080/Drogaria/rest/drogaria
+@Path("drogaria")
+public class DrogariaService {
+
+}
--- Drogaria/src/main/java/br/com/drogaria/util/DrogariaResourceConfig.java
+++ Drogaria/src/main/java/br/com/drogaria/util/DrogariaResourceConfig.java
@@ -1,7 +1,14 @@
 package br.com.drogaria.util;
 
+import javax.ws.rs.ApplicationPath;
+
 import org.glassfish.jersey.server.ResourceConfig;
 
+@ApplicationPath("rest")
 public class DrogariaResourceConfig extends ResourceConfig{
+	
+	public DrogariaResourceConfig(){
+		packages("br.com.drogaria.service");
+	}
 
 }
