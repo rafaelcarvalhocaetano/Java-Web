6be8e4d9710fc7a11c004c359917567700d4a730
Rafael <rapha.pse@outlook.com>
2017-05-02 20:47:46 -0300

    Criando imagemBean

--- Drogaria/src/main/java/br/com/drogaria/bean/FabricanteBean.java
+++ Drogaria/src/main/java/br/com/drogaria/bean/FabricanteBean.java
@@ -1,7 +1,6 @@
 package br.com.drogaria.bean;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
--- Drogaria/src/main/java/br/com/drogaria/bean/ImagemBean.java
+++ Drogaria/src/main/java/br/com/drogaria/bean/ImagemBean.java
@@ -1,6 +1,12 @@
 package br.com.drogaria.bean;
 
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 
 import org.primefaces.model.StreamedContent;
@@ -9,6 +15,7 @@ import org.primefaces.model.StreamedContent;
 @RequestScoped
 public class ImagemBean {
 	
+	@ManagedProperty("#{param.caminho}")
 	private String caminho;
 	private StreamedContent foto;
 	
@@ -19,6 +26,12 @@ public class ImagemBean {
 		this.caminho = caminho;
 	}
 	public StreamedContent getFoto() {
+		
+		if(caminho == null || caminho.isEmpty()){
+			Path foto = Paths.get("C:/javaFotos/code.png");
+			InputStream fluxo = Files.newInputStream(foto);
+		}
+		
 		return foto;
 	}
 	public void setFoto(StreamedContent foto) {
