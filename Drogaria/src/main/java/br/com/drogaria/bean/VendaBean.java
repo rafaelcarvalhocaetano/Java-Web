dd664342893b5ee1884e182b69854f5fbaa21a2d
Rafael <rapha.pse@outlook.com>
2017-05-05 19:40:16 -0300

    Adicionando botão de remover itens selecionados

--- Drogaria/src/main/java/br/com/drogaria/bean/VendaBean.java
+++ Drogaria/src/main/java/br/com/drogaria/bean/VendaBean.java
@@ -13,6 +13,8 @@ import javax.faces.event.ActionEvent;
 import org.omnifaces.util.Messages;
 
 import br.com.drogaria.dao.ProdutoDAO;
+import br.com.drogaria.domain.Cliente;
+import br.com.drogaria.domain.Funcionario;
 import br.com.drogaria.domain.ItemVenda;
 import br.com.drogaria.domain.Produto;
 import br.com.drogaria.domain.Venda;
@@ -20,33 +22,50 @@ import br.com.drogaria.domain.Venda;
 @SuppressWarnings("serial")
 @ManagedBean
 @ViewScoped
-public class VendaBean implements Serializable{
-	
-	
+public class VendaBean implements Serializable {
+
 	private Venda venda;
 	private List<Produto> produtos;
 	private List<ItemVenda> itensVendas;
-	
+	private List<Cliente> clientes;
+	private List<Funcionario> funcionarios;
+
 	public List<Produto> getProdutos() {
 		return produtos;
 	}
+
 	public void setProdutos(List<Produto> produtos) {
 		this.produtos = produtos;
 	}
-	
+
 	public List<ItemVenda> getItensVendas() {
 		return itensVendas;
 	}
+
 	public void setItensVendas(List<ItemVenda> itensVendas) {
 		this.itensVendas = itensVendas;
 	}
+
 	public Venda getVenda() {
 		return venda;
 	}
+
 	public void setVenda(Venda venda) {
 		this.venda = venda;
 	}
-	
+	public List<Cliente> getClientes() {
+		return clientes;
+	}
+	public void setClientes(List<Cliente> clientes) {
+		this.clientes = clientes;
+	}
+	public List<Funcionario> getFuncionarios() {
+		return funcionarios;
+	}
+	public void setFuncionarios(List<Funcionario> funcionarios) {
+		this.funcionarios = funcionarios;
+	}
+
 	@PostConstruct
 	public void novo() {
 
@@ -56,7 +75,7 @@ public class VendaBean implements Serializable{
 
 			ProdutoDAO dao = new ProdutoDAO();
 			produtos = dao.listar("descricao");
-			
+
 			itensVendas = new ArrayList<>();
 
 			Messages.addGlobalInfo("Sucesso");
@@ -66,6 +85,7 @@ public class VendaBean implements Serializable{
 		}
 
 	}
+
 	public void adicionar(ActionEvent evento) {
 		Produto produto = (Produto) evento.getComponent().getAttributes().get("produtoSelecionado");
 
@@ -81,30 +101,40 @@ public class VendaBean implements Serializable{
 			itemVenda.setValorParcial(produto.getPreco());
 			itemVenda.setProduto(produto);
 			itemVenda.setQuantidade(new Short("1"));
-			
+
 			itensVendas.add(itemVenda);
-		}else{
+		} else {
 			ItemVenda itemVenda = itensVendas.get(achou);
 			itemVenda.setQuantidade(new Short(itemVenda.getQuantidade() + 1 + ""));
 			itemVenda.setValorParcial(produto.getPreco().multiply(new BigDecimal(itemVenda.getQuantidade())));
-		}	
+		}
+		
+		calcular();
 	}
+
 	public void remover(ActionEvent evento) {
 		ItemVenda itemVenda = (ItemVenda) evento.getComponent().getAttributes().get("itemSelecionado");
-		
+
 		int achou = -1;
-		for(int posicao = 0; posicao < itensVendas.size(); posicao++){
-			if(itensVendas.get(posicao).getProduto().equals(itemVenda.getProduto())){
+		for (int posicao = 0; posicao < itensVendas.size(); posicao++) {
+			if (itensVendas.get(posicao).getProduto().equals(itemVenda.getProduto())) {
 				achou = posicao;
 			}
 		}
-		
-		if(achou > -1){
+
+		if (achou > -1) {
 			itensVendas.remove(achou);
 		}
-	}
-	public void calcular(){
-		
+		calcular();
 	}
 
+	public void calcular() {
+		venda.setPrecoTotal(new BigDecimal("0.00"));
+
+		for (int i = 0; i < itensVendas.size(); i++) {
+			ItemVenda itemVenda = itensVendas.get(i);
+			venda.setPrecoTotal(venda.getPrecoTotal().add(itemVenda.getValorParcial()));
+		}
+
+	}
 }
