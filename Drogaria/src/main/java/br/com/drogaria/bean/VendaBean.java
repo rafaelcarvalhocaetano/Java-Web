a3a6515abd0f90e9a4f89c148d7e3c868df3ac82
Rafael <rapha.pse@outlook.com>
2017-05-05 13:35:44 -0300

    Adicionando tela de vendas

--- Drogaria/src/main/java/br/com/drogaria/bean/VendaBean.java
+++ Drogaria/src/main/java/br/com/drogaria/bean/VendaBean.java
@@ -9,8 +9,8 @@ import javax.faces.bean.ViewScoped;
 
 import org.omnifaces.util.Messages;
 
-import br.com.drogaria.dao.FabricanteDAO;
 import br.com.drogaria.dao.ProdutoDAO;
+import br.com.drogaria.domain.ItemVenda;
 import br.com.drogaria.domain.Produto;
 
 @SuppressWarnings("serial")
@@ -19,12 +19,20 @@ import br.com.drogaria.domain.Produto;
 public class VendaBean implements Serializable{
 	
 	private List<Produto> produtos;
+	private List<ItemVenda> itensVendas;
 	
 	public List<Produto> getProdutos() {
 		return produtos;
 	}
 	public void setProdutos(List<Produto> produtos) {
 		this.produtos = produtos;
+	}
+	
+	public List<ItemVenda> getItensVendas() {
+		return itensVendas;
+	}
+	public void setItensVendas(List<ItemVenda> itensVendas) {
+		this.itensVendas = itensVendas;
 	}
 	
 	@PostConstruct
