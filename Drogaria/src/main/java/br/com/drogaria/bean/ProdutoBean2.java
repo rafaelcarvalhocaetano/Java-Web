ed4e9a626675dc65091f01f2dbe66bdcb1ca7000
Rafael <rapha.pse@outlook.com>
2017-05-10 20:55:19 -0300

    Adicionando pagina de pesquisa por produto específico

--- Drogaria/src/main/java/br/com/drogaria/bean/ProdutoBean2.java
+++ Drogaria/src/main/java/br/com/drogaria/bean/ProdutoBean2.java
@@ -1,10 +1,29 @@
 package br.com.drogaria.bean;
 
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import br.com.drogaria.domain.Produto;
+
+@SuppressWarnings("serial")
 @ManagedBean
 @ViewScoped
-public class ProdutoBean2 {
+public class ProdutoBean2 implements Serializable{
+	private Produto produto;
+	
+	public Produto getProduto() {
+		return produto;
+	}
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	
+	@PostConstruct
+	public void novo(){
+		produto = new Produto();
+	}
 
 }
