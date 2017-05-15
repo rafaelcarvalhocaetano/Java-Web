006d61ae0b5047e40f2d011eb3a0b216907ca117
Rafael <rapha.pse@outlook.com>
2017-05-15 20:54:25 -0300

    Adicionando projeto Bhaskara - HELP

---
+++ MathBhaskara/src/br/uninove/edu/CalculandoBhaskara.java
@@ -0,0 +1,33 @@
+package br.uninove.edu;
+
+public class CalculandoBhaskara {
+	
+	private String a,b,c;
+
+	public String getA() {
+		return a;
+	}
+
+	public void setA(String a) {
+		this.a = a;
+	}
+
+	public String getB() {
+		return b;
+	}
+
+	public void setB(String b) {
+		this.b = b;
+	}
+
+	public String getC() {
+		return c;
+	}
+
+	public void setC(String c) {
+		this.c = c;
+	}
+	
+	
+
+}
