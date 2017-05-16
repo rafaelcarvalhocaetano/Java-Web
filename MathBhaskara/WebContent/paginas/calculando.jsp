b991f1b7f28adaf3f78a8994a321827318cefee5
Rafael <rapha.pse@outlook.com>
2017-05-15 21:00:52 -0300

    Adicionando projeto Bhaskara - HELP

--- MathBhaskara/WebContent/paginas/calculando.jsp
+++ MathBhaskara/WebContent/paginas/calculando.jsp
@@ -1,3 +1,4 @@
+<%@page import="br.uninove.edu.CalculandoBhaskara"%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -7,6 +8,8 @@
 <title>Resultado</title>
 </head>
 <body>
+
+<jsp:useBean scope="page" id="cal" class="br.uninove.edu.CalculandoBhaskara"></jsp:useBean>
 
 
 
--- MathBhaskara/src/br/uninove/edu/CalculandoBhaskara.java
+++ MathBhaskara/src/br/uninove/edu/CalculandoBhaskara.java
@@ -28,6 +28,62 @@ public class CalculandoBhaskara {
 		this.c = c;
 	}
 	
+	public double delta() {
+
+		try {
+			double valorA, valorB, valorC;
+
+			valorA = Double.parseDouble(a);
+			valorB = Double.parseDouble(b);
+			valorC = Double.parseDouble(c);
+
+			double delta = (valorB * valorB) - 4 * valorA * valorC;
+			return delta;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		}
+
+	}
 	
+	public String raizPositiva(){
+		try {
+			double valorA, valorB;
+
+			valorA = Double.parseDouble(a);
+			valorB = Double.parseDouble(b);
+			
+			if(delta() < 0 ){
+				return "Delta é negativo";
+			}else{
+				double raizPositiva;
+				raizPositiva = ((-valorB + Math.sqrt(delta())) / (2 * valorA));
+				return String.valueOf(raizPositiva);
+			}
+
+		} catch (Exception e) {
+			return "Erro";
+		}
+
+	}
+	public String raizNegativa(){
+		try {
+			double valorA, valorB;
+
+			valorA = Double.parseDouble(a);
+			valorB = Double.parseDouble(b);
+			
+			if(delta() < 0 ){
+				return "Delta é negativo";
+			}else{
+				double raizPositiva;
+				raizPositiva = (( -valorB - Math.sqrt(delta()) ) / (2 * valorA));
+				return String.valueOf(raizPositiva);
+			}
+		} catch (Exception e) {
+			return "Erro";
+		}
+	}
 
 }
