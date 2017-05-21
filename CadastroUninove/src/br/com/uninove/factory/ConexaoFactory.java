9a4a6932af151248a973ee4861a957148b6e8c0e
Rafael <rapha.pse@outlook.com>
2017-05-21 20:35:05 -0300

    Adicionando Banco de Dados

--- CadastroUninove/src/br/com/uninove/factory/ConexaoFactory.java
+++ CadastroUninove/src/br/com/uninove/factory/ConexaoFactory.java
@@ -1,7 +1,27 @@
 package br.com.uninove.factory;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexaoFactory {
-	//cria conexão com o banco de dados
+		
+	private static final String USUARIO = "root";
+	private static final String URL = "dbc:mysql://localhost:3306/drogaria";
+	private static final String SENHA = "q1w2e3r4";
+	
+	public static Connection conectar() throws SQLException{
+		try {
+			
+			DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+						
+		} catch (Exception e) {
+			System.out.println("Erro de conexão");
+		}
+		Connection connection = DriverManager.getConnection(URL, USUARIO, SENHA);
+		
+		return connection;
+	}
 	
 	
 }
