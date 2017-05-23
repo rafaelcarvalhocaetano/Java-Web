15cf4c3220c828459301cacc2b51a2f8f603de74
Rafael <rapha.pse@outlook.com>
2017-05-23 19:25:07 -0300

    Adicionando Servlet no Cadastro

---
+++ CadastroUninove/src/br/com/uninove/servlet/CrudCadastro.java
@@ -0,0 +1,38 @@
+package br.com.uninove.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/CrudCadastro")
+public class CrudCadastro extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public CrudCadastro() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
