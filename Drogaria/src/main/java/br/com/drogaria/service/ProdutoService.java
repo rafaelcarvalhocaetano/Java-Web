9f69e681aeb056ffa3f17a4df894b04b6873a3bd
Rafael <rapha.pse@outlook.com>
2017-05-29 15:23:27 -0300

    Continuando com RESTFUL

--- Drogaria/src/main/java/br/com/drogaria/service/ProdutoService.java
+++ Drogaria/src/main/java/br/com/drogaria/service/ProdutoService.java
@@ -3,6 +3,7 @@ package br.com.drogaria.service;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
 import com.google.gson.Gson;
@@ -24,6 +25,20 @@ public class ProdutoService {
 		
 		String json = gson.toJson(produtos);
 		return json;
+	}
+	
+	@POST
+	public String salvar(String json){
+		
+		Gson gson = new Gson();
+		Produto produto = gson.fromJson(json, Produto.class);
+		
+		ProdutoDAO dao = new ProdutoDAO();
+		produto = dao.merge(produto);
+		
+		String saida = gson.toJson(produto);
+		return saida;
+		
 	}
 
 }
