ae1d4867b0967b5e77be744cc9ca304525d12125
Rafael <rapha.pse@outlook.com>
2017-05-29 15:02:52 -0300

    Continuando com RESTFUL

---
+++ Drogaria/src/main/java/br/com/drogaria/service/ProdutoService.java
@@ -0,0 +1,29 @@
+package br.com.drogaria.service;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import com.google.gson.Gson;
+
+import br.com.drogaria.dao.ProdutoDAO;
+import br.com.drogaria.domain.Produto;
+
+//http://127.0.0.1:8080/Drogaria/rest/produto
+
+@Path("produto")
+public class ProdutoService {
+	
+	@GET
+	public String listar(){
+		ProdutoDAO dao = new ProdutoDAO();
+		List<Produto> produtos = dao.listar();
+		
+		Gson gson = new Gson();
+		
+		String json = gson.toJson(produtos);
+		return json;
+	}
+
+}
