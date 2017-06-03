bb0bde19fd60d45d5ce8ee83a3bdff5de96028eb
Rafael <rapha.pse@outlook.com>
2017-06-03 20:59:53 -0300

    Adicionando Conf do Hibernate

--- THibernate/src/main/java/com/hibernate/domain/Login.java
+++ THibernate/src/main/java/com/hibernate/domain/Login.java
@@ -1,17 +1,12 @@
 package com.hibernate.domain;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
 
 @Entity
-public class Login {
+public class Login extends GenericDomain{
 	
 		
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private int codigo;
 	
 	
 	private String nome;
