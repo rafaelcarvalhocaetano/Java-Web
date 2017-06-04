6a5b74a2c5c24b3508157542a0b32bdee2e6a091
Rafael <rapha.pse@outlook.com>
2017-06-03 21:59:09 -0300

    Adicionando Conf do Hibernate

--- THibernate/src/main/java/com/hibernate/dao/GenericDAO.java
+++ THibernate/src/main/java/com/hibernate/dao/GenericDAO.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 
 import com.hibernate.util.HibernateUtil;
 
@@ -60,6 +61,7 @@ public class GenericDAO<Entidade> {
 		}
 	}
 	
+	//Lista todos os usuários cadastrados
 	@SuppressWarnings("unchecked")
 	public List<Entidade> listar(){
 		
@@ -69,6 +71,27 @@ public class GenericDAO<Entidade> {
 			
 			Criteria consulta = sessao.createCriteria(classe);
 			List<Entidade> resultado = consulta.list();			
+			return resultado;
+			
+		} catch (RuntimeException e) {
+			throw e;
+		}finally {
+			sessao.close();
+		}
+	}
+	
+	//busca através de código
+
+	@SuppressWarnings("unchecked")
+	public Entidade buscar(Long codigo){
+		
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
+		
+		try {
+			
+			Criteria consulta = sessao.createCriteria(classe);
+			consulta.add(Restrictions.idEq(codigo));
+			Entidade resultado = (Entidade) consulta.uniqueResult();
 			return resultado;
 			
 		} catch (RuntimeException e) {
--- THibernate/src/test/java/com/hibernate/test/dao/LoginDaoTest.java
+++ THibernate/src/test/java/com/hibernate/test/dao/LoginDaoTest.java
@@ -23,6 +23,7 @@ public class LoginDaoTest {
 	}
 	
 	@Test
+	@Ignore
 	public void listar(){
 		
 		LoginDAO dao = new LoginDAO();
@@ -32,6 +33,21 @@ public class LoginDaoTest {
 			System.out.println(login.getCodigo());
 			System.out.println(login.getNome()+" "+login.getSobreNome());
 		}
+	}
+	@Test
+	public void buscar(){
+		Long codigo = 2L;
+		
+		LoginDAO dao = new LoginDAO();
+		Login login = dao.buscar(codigo);
+		
+		if(login == null){
+			System.out.println("Nenhum resultado encontrado ...");
+		}else{
+			System.out.println(login.getCodigo()+" - "+login.getNome()+" "+login.getSobreNome());
+		}
+		
+		
 	}
 
 }
