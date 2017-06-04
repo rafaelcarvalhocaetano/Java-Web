6fc0c8a61f7b1a4d72ba714d96d116ed7719a1dd
Rafael <rapha.pse@outlook.com>
2017-06-04 20:30:42 -0300

    Adicionando estilo primeface e modificações com css

--- THibernate/src/main/java/com/hibernate/bean/LoginBean.java
+++ THibernate/src/main/java/com/hibernate/bean/LoginBean.java
@@ -1,9 +1,12 @@
 package com.hibernate.bean;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.view.facelets.FaceletContext;
 
 import com.hibernate.domain.Login;
 
@@ -27,6 +30,15 @@ public class LoginBean implements Serializable{
 		this.logins = logins;
 	}
 	
+	public void navegar(){
+		
+		try {
+			FacesContext.getCurrentInstance().getExternalContext().redirect("logar.xhtml");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
 	
 	
 	
