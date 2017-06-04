cf1dbfb128e24b659fe914b1c51d1678c9854702
Rafael <rapha.pse@outlook.com>
2017-06-03 22:20:43 -0300

    Adicionando Conf do Hibernate

--- THibernate/src/main/java/com/hibernate/bean/LoginBean.java
+++ THibernate/src/main/java/com/hibernate/bean/LoginBean.java
@@ -1,7 +1,33 @@
 package com.hibernate.bean;
 
+import java.io.Serializable;
+import java.util.List;
 
-public class LoginBean {
+
+import com.hibernate.domain.Login;
+
+
+@SuppressWarnings("serial")
+
+public class LoginBean implements Serializable{
+	
+	private Login login;
+	private List<Login> logins;
+	
+	public Login getLogin() {
+		return login;
+	}
+	public void setLogin(Login login) {
+		this.login = login;
+	}
+	public List<Login> getLogins() {
+		return logins;
+	}
+	public void setLogins(List<Login> logins) {
+		this.logins = logins;
+	}
+	
+	
 	
 
 }
